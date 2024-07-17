77959b01e6ce031881b7a34bcdd7f5df05d1441d
beltik <smash.must.die@gmail.com>
2024-07-17 16:26:31 +0300

    Getting first values to business entity

--- src/main/java/com/liga/orsr/ORSRParser.java
+++ src/main/java/com/liga/orsr/ORSRParser.java
@@ -1,5 +1,6 @@
 package com.liga.orsr;
 
+import com.liga.orsr.model.LegalEntity;
 import com.machinepublishers.jbrowserdriver.JBrowserDriver;
 import com.machinepublishers.jbrowserdriver.Settings;
 import com.machinepublishers.jbrowserdriver.Timezone;
@@ -10,22 +11,48 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.function.Consumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ORSRParser {
+    public static final String REGEX_COURT = "Extract from the.+?(Court .+)";
+    public static final String REGEX_INSERT = "Insert No.:\\\\n(.+?)\\\\n";
+    public static final String REGEX_BUSINESS_NAME = "Business name:\\\\n(.+?)\\\\n";
 
     public static void parseFiveK() throws IOException {
         JBrowserDriver driver = new JBrowserDriver(Settings
                 .builder().
                 timezone(Timezone.EUROPE_KIEV).build());
-        driver.get("https://www.orsr.sk/vypis.asp?lan=en&ID=1&SID=9&P=1");
-        String loadedPage = driver.getPageSource();
-        Document doc = Jsoup.parse(loadedPage);
-        doc.select("span").addClass("ra").forEach(System.out::println);
-        Elements scriptTags = doc.getElementsByTag("script");
-        for (Element tag : scriptTags){
-            for (DataNode node : tag.dataNodes()) {
-                System.out.println(node.getWholeData());
-            }
+        try {
+            driver.get("https://www.orsr.sk/vypis.asp?lan=en&ID=1&SID=9&P=1");
+            String loadedPage = driver.getPageSource();
+            Document doc = Jsoup.parse(loadedPage);
+            LegalEntity currentEntity = new LegalEntity();
+            currentEntity.setCourt(getPatternFromText(doc.select("td:not(:has(img))").first().text(), REGEX_COURT));
+            System.out.println(currentEntity.getCourt());
+            StringBuilder allInfo = new StringBuilder();
+            doc.select("span").forEach(node ->
+                allInfo.append(node.text()).append("\\n")
+            );
+            System.out.println(allInfo);
+            currentEntity.setInsertNumber(getPatternFromText(allInfo.toString(), REGEX_INSERT));
+            System.out.println(currentEntity.getInsertNumber());
+            currentEntity.setBusinessName(getPatternFromText(allInfo.toString(), REGEX_BUSINESS_NAME));
+            System.out.println(currentEntity.getBusinessName());
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            driver.quit();
         }
+    }
+
+    private static String getPatternFromText(String text, String pattern) {
+        Pattern regex = Pattern.compile(pattern);
+        Matcher matcher = regex.matcher(text);
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+        return "";
     }
 }
--- src/main/java/com/liga/orsr/model/LegalEntity.java
+++ src/main/java/com/liga/orsr/model/LegalEntity.java
@@ -2,17 +2,87 @@ package com.liga.orsr.model;
 
 import java.time.LocalDate;
 
-public class LegalEntity
-//        (
-//        Long identificationNumber,
-//        String insertNumber,
-//        String court,
-//        String businessName,
-//        String registeredAddress,
-//        LocalDate entryDate,
-//        LocalDate deletionDate,
-//        String legalForm,
-//        String jsonAllOtherInfo
-//                          )
-{
+public class LegalEntity {
+
+    Long identificationNumber;
+    String insertNumber;
+    String court;
+    String businessName;
+    String registeredAddress;
+    LocalDate entryDate;
+    LocalDate deletionDate;
+    String legalForm;
+    String jsonAllOtherInfo;
+
+    public Long getIdentificationNumber() {
+        return identificationNumber;
+    }
+
+    public void setIdentificationNumber(Long identificationNumber) {
+        this.identificationNumber = identificationNumber;
+    }
+
+    public String getInsertNumber() {
+        return insertNumber;
+    }
+
+    public void setInsertNumber(String insertNumber) {
+        this.insertNumber = insertNumber;
+    }
+
+    public String getCourt() {
+        return court;
+    }
+
+    public void setCourt(String court) {
+        this.court = court;
+    }
+
+    public String getBusinessName() {
+        return businessName;
+    }
+
+    public void setBusinessName(String businessName) {
+        this.businessName = businessName;
+    }
+
+    public String getRegisteredAddress() {
+        return registeredAddress;
+    }
+
+    public void setRegisteredAddress(String registeredAddress) {
+        this.registeredAddress = registeredAddress;
+    }
+
+    public LocalDate getEntryDate() {
+        return entryDate;
+    }
+
+    public void setEntryDate(LocalDate entryDate) {
+        this.entryDate = entryDate;
+    }
+
+    public LocalDate getDeletionDate() {
+        return deletionDate;
+    }
+
+    public void setDeletionDate(LocalDate deletionDate) {
+        this.deletionDate = deletionDate;
+    }
+
+    public String getLegalForm() {
+        return legalForm;
+    }
+
+    public void setLegalForm(String legalForm) {
+        this.legalForm = legalForm;
+    }
+
+    public String getJsonAllOtherInfo() {
+        return jsonAllOtherInfo;
+    }
+
+    public void setJsonAllOtherInfo(String jsonAllOtherInfo) {
+        this.jsonAllOtherInfo = jsonAllOtherInfo;
+    }
 }
